b1758a0cb3e9c137d7f4840014b8b90fd3b9a3c5
Joel Andres Granados <joel.granados@gmail.com>
2012-02-07 22:00:10 +0100

    Change var names to increase readability

--- 2-make_probabilities_from_frequencies.py
+++ 2-make_probabilities_from_frequencies.py
@@ -74,8 +74,8 @@ def calcProb ( fgFile="", bgFile="" ):
     print ( "Calculating probability for %s" % os.path.split(fgFile)[1] )
 
     # load up the first list with color value and frequency
-    firstCountDict = {} # count of pixels
-    firstDictionary = {} # frequency of pixels
+    fgCountDict = {} # count of pixels
+    fgFrecDict = {} # frequency of pixels
 
     #  open the foreground data file
     fdTmp = open(fgFile, 'rb')
@@ -86,118 +86,118 @@ def calcProb ( fgFile="", bgFile="" ):
     for fgRow in fgReader:
         if len(fgRow) == 3:
             tmpkey = float(fgRow[0])
-            firstCountDict[tmpkey] = int(float(fgRow[1]))
-            firstDictionary[tmpkey] = float(fgRow[2])
+            fgCountDict[tmpkey] = int(float(fgRow[1]))
+            fgFrecDict[tmpkey] = float(fgRow[2])
 
         elif len(fgRow) == 4:
             tmpkey = (float(fgRow[0]),float(fgRow[1]))
-            firstCountDict[tmpkey] = float(fgRow[2])
-            firstDictionary[tmpkey] = float(fgRow[3])
+            fgCountDict[tmpkey] = float(fgRow[2])
+            fgFrecDict[tmpkey] = float(fgRow[3])
 
         elif len(fgRow) == 5:
             tmpkey = ( float(fgRow[0]), float(fgRow[1]), float(fgRow[2]) )
-            firstCountDict[tmpkey] = float(fgRow[3])
-            firstDictionary[tmpkey] = float(fgRow[4])
+            fgCountDict[tmpkey] = float(fgRow[3])
+            fgFrecDict[tmpkey] = float(fgRow[4])
 
     fdTmp.close()
 
     # over-write the original file!!!
-    writerFirstFile = open(fgFile, 'wb')
-    writer = csv.writer(writerFirstFile, delimiter = ',',quoting=csv.QUOTE_NONE)
+    fgFd = open(fgFile, 'wb')
+    fgWriter = csv.writer(fgFd, delimiter = ',',quoting=csv.QUOTE_NONE)
 
     # open the second data file
-    readerSecondFile = open(bgFile, 'rb')
-    secondReader = csv.reader(readerSecondFile, delimiter = ',')
+    bgFd = open(bgFile, 'rb')
+    bgReader = csv.reader(bgFd, delimiter = ',')
 
     newList = [ 'Frequency', 'Probs_vs_%s'%bgFile.split('/')[-4], 'Count' ]
-    secondHeader = secondReader.next()
+    secondHeader = bgReader.next()
     if len(secondHeader) >= 3:
         newList.insert(0, secondHeader[0])
     if len(secondHeader) >= 4:
         newList.insert(1, secondHeader[1])
     if len(secondHeader) >= 5:
         newList.insert(2, secondHeader[2])
-    writer.writerow(newList)
+    fgWriter.writerow(newList)
 
     # Loop through the second file and match it with the items in
     # the first file, creating a probability [0:1]
-    for secondDataItem in secondReader:
+    for bgRow in bgReader:
         # if the color space is a single vector
-        if len(secondDataItem) == 3:
-            index = float(secondDataItem[0])
+        if len(bgRow) == 3:
+            index = float(bgRow[0])
             # If the background item is in the first dictionary,
             # then make the probability
-            if index in firstDictionary:
+            if index in fgFrecDict:
                 newList = [index]  #  write the color vector value
 
                 # write the frequency of the value
                 # (number of pixels / total count)
-                newList.append(firstDictionary[index])
+                newList.append(fgFrecDict[index])
 
                 # write frequency/frequency of background for that color
-                newList.append( firstDictionary[index]
-                                / ( float(secondDataItem[2])
-                                    + firstDictionary[index] ) )
+                newList.append( fgFrecDict[index]
+                                / ( float(bgRow[2])
+                                    + fgFrecDict[index] ) )
 
                 # write original count
-                newList.append(firstCountDict[index])
-                writer.writerow(newList)
+                newList.append(fgCountDict[index])
+                fgWriter.writerow(newList)
                 # remove the item from the first dictionary
                 # to keep track of things
-                del firstDictionary[index]
+                del fgFrecDict[index]
             else:
                 # if the item is not in the first dictionary,
                 # then write the probability of zero
-                writer.writerow([index, float(secondDataItem[2]), 0.0, 0])
+                fgWriter.writerow([index, float(bgRow[2]), 0.0, 0])
 
         # if the color space is a double
-        elif len(secondDataItem) == 4:
-            index = (float(secondDataItem[0]), float(secondDataItem[1]))
-            if index in firstDictionary:
+        elif len(bgRow) == 4:
+            index = (float(bgRow[0]), float(bgRow[1]))
+            if index in fgFrecDict:
                 #  write the color vector values
                 newList = [index[0], index[1]]
-                newList.append(firstDictionary[index])
-                newList.append( firstDictionary[index]
-                                / ( float(secondDataItem[3])
-                                    + firstDictionary[index] ) )
-                newList.append(firstCountDict[index])
-                writer.writerow(newList)
-                del firstDictionary[index]
+                newList.append(fgFrecDict[index])
+                newList.append( fgFrecDict[index]
+                                / ( float(bgRow[3])
+                                    + fgFrecDict[index] ) )
+                newList.append(fgCountDict[index])
+                fgWriter.writerow(newList)
+                del fgFrecDict[index]
 
             else:
-                writer.writerow([float(secondDataItem[0]),
-                                 float(secondDataItem[1]),
-                                 float(secondDataItem[3]),
-                                 0.0,
-                                 0])
+                fgWriter.writerow([float(bgRow[0]),
+                                   float(bgRow[1]),
+                                   float(bgRow[3]),
+                                   0.0,
+                                   0])
 
         # if the color space is a triple
-        elif len(secondDataItem) == 5:
-            index = ( float(secondDataItem[0]),
-                      float(secondDataItem[1]),
-                      float(secondDataItem[2]) )
-            if index in firstDictionary:
+        elif len(bgRow) == 5:
+            index = ( float(bgRow[0]),
+                      float(bgRow[1]),
+                      float(bgRow[2]) )
+            if index in fgFrecDict:
                 # write the color vector values
                 newList = [index[0], index[1], index[2]]
-                newList.append(firstDictionary[index])
-                newList.append(firstDictionary[index]
-                               / ( float(secondDataItem[4])
-                                   + firstDictionary[index] ) )
-                newList.append(firstCountDict[index])
-                writer.writerow(newList)
-                del firstDictionary[index]
+                newList.append(fgFrecDict[index])
+                newList.append(fgFrecDict[index]
+                               / ( float(bgRow[4])
+                                   + fgFrecDict[index] ) )
+                newList.append(fgCountDict[index])
+                fgWriter.writerow(newList)
+                del fgFrecDict[index]
             else:
-                writer.writerow([float(secondDataItem[0]),
-                                 float(secondDataItem[1]),
-                                 float(secondDataItem[2]),
-                                 float(secondDataItem[3]),
+                fgWriter.writerow([float(bgRow[0]),
+                                 float(bgRow[1]),
+                                 float(bgRow[2]),
+                                 float(bgRow[3]),
                                  0.0,
                                  0])
 
     # for the remaining items that were in the first
     # list but did not occur in the second, write them
     # to the file wiht a probablity of 1
-    for items in firstDictionary.iteritems():
+    for items in fgFrecDict.iteritems():
         newList = []
         # if the first item in the dictionary is a tuple
         # (it is "iterable"), then start with that tuple
@@ -210,11 +210,11 @@ def calcProb ( fgFile="", bgFile="" ):
         newList.append(items[1])  # append the frequency
         newList.append(1.0)  #  append '1'
         # append the count of the foreground
-        newList.append(firstCountDict[items[0]])
-        writer.writerow(newList)
+        newList.append(fgCountDict[items[0]])
+        fgWriter.writerow(newList)
 
-    writerFirstFile.close()
-    readerSecondFile.close()
+    fgFd.close()
+    bgFd.close()
 
 if __name__ == "__main__":
     main()
