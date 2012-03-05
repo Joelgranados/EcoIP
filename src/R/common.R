0ae62f64dea8691ce0b79d2172a660ef6f7a6147
Joel Andres Granados <joel.granados@gmail.com>
2012-03-05 19:43:52 +0100

    Separate logic in different files.
    Also add licensing text that was missing.
    * src/R/common.R: Remove model logic from this file. This will only contain
    common stuff.
    * src/R/naiveBayes.R: New file containing all the model logic.

--- src/R/common.R
+++ src/R/common.R
@@ -1,3 +1,19 @@
+# Copyright (C) 2012 Joel Granados <joel.granados@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 getCSV <- function(filename)
 {
     if ( !file.exists(filename) )
@@ -150,91 +166,6 @@ getPixels <- function(directory, label)
         return (retError ("Failed to accumulate any pixels."))
 
     return (pixAccum)
-}
-
-# Calc the Naive Bayesian element. In P(a|b) = (prod(P(b|a))*p(a))/p(b) we are
-# calculating P(b|a).
-# FIXME: We are concerned about R's pass by value. The link found at
-#   http://cran.r-project.org/doc/manuals/R-lang.html#Argument-evaluation
-#   suggests that the colMat matrix will not be recopied.
-calcNaiveBayesElem <- function(colMat, bins)
-{
-    histlist = list()
-    colMat = as.matrix(colMat)
-
-    for (i in 1:dim(colMat)[2])
-        histlist[[i]] = hist(colMat[,i], bins, plot=FALSE)
-
-    if ( length(histlist) == 0 )
-        return (retError("Could not histlist"))
-
-    return (histlist)
-}
-
-# Create a Discrete Naive Bayesian Model
-create.DiscNaiveBayesianModel <- function(classes, dataPoints, numBins)
-{
-    if ( !is.matrix(dataPoints) )
-        return (retError ( "The dataPoints argument must be a matrix" ))
-
-    if ( !is.vector(classes) )
-        return (retError ( "The classes argument must be a boolean vector" ))
-
-    if ( length(classes) != dim(dataPoints)[1] )
-        return (retError("Classes length must be equal to first dim dataPoints"))
-
-    if ( sum(classes) == 0 || sum(!classes) == 0 )
-        return (retError ("Must include data for two classes"))
-
-    if ( class(classes) != "logical")
-        return (retError ("Classes must be a logical vector"))
-
-    NBM = list() #Naive Bayesian Model (NBM)
-    NBM$bins = seq(0,1,1/numBins)
-    NBM$cls1Hists = calcNaiveBayesElem(dataPoints[classes,],NBM$bins)
-    NBM$cls0Hists = calcNaiveBayesElem(dataPoints[!classes,],NBM$bins)
-
-    NBM$freq1 = sum(classes)/length(classes)
-    NBM$freq0 = sum(!classes)/length(classes)
-
-    NBM$dimension = dim(dataPoints)[2]
-
-    return (NBM)
-}
-
-# Classify with discrete Naive Bayesian Model
-classify.DiscNaiveBayesianModel <- function(NBM, dataInput)
-{
-    nbmNames = names(NBM)
-    if ( is.null(nbmNames)
-         || !"cls1Hists" %in% nbmNames || !"cls0Hists" %in% nbmNames
-         || !"freq1" %in% nbmNames || !"freq0" %in% nbmNames
-         || !"dimension" %in% nbmNames || !"bins" %in% nbmNames )
-        return (retError("The NBM object is not a Naive Bayesian Model Object"))
-
-    if ( dim(dataInput)[2] != NBM$dimension )
-        return (retError("The dimensions of data and model should be thesame"))
-
-    # Fit the raw data into the bins.
-    for (i in 1:dim(dataInput)[2])
-        dataInput[,i] = findInterval(dataInput[,i] , NBM$bins)
-
-    # OneZero[,1] -> One probabilities | OneZero[,2] -> Zero Probabilities.
-    OneZero = matrix( rep(1,dim(dataInput)[1]*2),
-                      ncol=2, nrow=dim(dataInput)[1] )
-
-    # Calculate the One probabilities.
-    for (i in 1:dim(dataInput)[2])
-        OneZero[,1] = OneZero[,1] * NBM$cls1Hists[[i]]$density[dataInput[,i]]
-    OneZero[,1] = OneZero[,1] * NBM$freq1
-
-    # Calculate the Zero probabilities.
-    for (i in 1:dim(dataInput)[2])
-        OneZero[,2] = OneZero[,2] * NBM$cls0Hists[[i]]$density[dataInput[,i]]
-    OneZero[,2] = OneZero[,2] * NBM$freq0
-
-    # Return the classification.
-    return(OneZero[,1] > OneZero[,2])
 }
 
 # Helper function.
