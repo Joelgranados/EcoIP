441df08087a807af26d1d79f24aadd3d8897d5ba
Joel Andres Granados <joel.granados@gmail.com>
2012-03-09 03:11:23 +0100

    Implement pass by reference in the color Transformations

--- src/R/colorTrans.R
+++ src/R/colorTrans.R
@@ -20,57 +20,104 @@
 #   http://cran.r-project.org/doc/manuals/R-lang.html#Argument-evaluation
 #   suggests that the colMat matrix will not be recopied.
 
-rgb2rgb <-function( RGB )
+passByRefMessage <- function(mess)
 {
+    return (
+        paste("To control memory usage we have implemented pass by reference ",
+            "by using R's environments. When calling one of these functions, ",
+            "first put all of the expected arguments in a new environment ",
+            "called refArgs. Note that at the end of the function those ",
+            "arguments will be removed. If they are still referenced in other ",
+            "environments, they will not be garbage collected. ?new.env, "
+            "?assign, ?rm, ?get for more information."
+            "Error: ", mess)
+        )
+}
+
+rgb2rgb <-function()
+{
+    #FIXME: We should check if environment is valid.
+    if ( ! "RGB" %in% ls(envir=as.environment(refArgs)) )
+        stop (passByRefMessage("The rgb2rgb method needs RGB"))
+    RGB = get("RGB", envir=as.environment(refArgs))
+
     if ( dim(RGB)[2] != 3 )
         stop ("The rgb var must have 3 dimensions")
 
+    rm("RGB", envir=as.environment(refArgs))
     return (RGB)
 }
 
-rgb2r <- function( RGB )
+rgb2r <- function()
 {
+    #FIXME: We should check if environment is valid.
+    if ( ! "RGB" %in% ls(envir=as.environment(refArgs)) )
+        stop (passByRefMessage("The rgb2r method needs RGB"))
+    RGB = get("RGB", envir=as.environment(refArgs))
+
     if ( dim(RGB)[2] != 3 )
         stop ("The rgb var must have 3 dimensions")
 
+    rm("RGB", envir=as.environment(refArgs))
     return (RGB[,1])
 }
 
-rgb2g <- function( RGB )
+rgb2g <- function()
 {
+    #FIXME: We should check if environment is valid.
+    if ( ! "RGB" %in% ls(envir=as.environment(refArgs)) )
+        stop (passByRefMessage("The rgb2g method needs RGB"))
+    RGB = get("RGB", envir=as.environment(refArgs))
+
     if ( dim(RGB)[2] != 3 )
         stop ("The rgb var must have 3 dimensions")
 
+    rm("RGB", envir=as.environment(refArgs))
     return (RGB[,2])
 }
 
-rgb2b <- function( RGB )
+rgb2b <- function()
 {
+    #FIXME: We should check if environment is valid.
+    if ( ! "RGB" %in% ls(envir=as.environment(refArgs)) )
+        stop (passByRefMessage("The rgb2b method needs RGB"))
+    RGB = get("RGB", envir=as.environment(refArgs))
+
     if ( dim(RGB)[2] != 3 )
         stop ("The rgb var must have 3 dimensions")
 
+    rm("RGB", envir=as.environment(refArgs))
     return (RGB[,3])
 }
 
-# We assume dim(rgb)=c(N,3). rgb[1]->R, rgb[2]->g, rgb[3]->b
 # We base our calculations on opencv's equation.
 # http://opencv.itseez.com/modules/imgproc/doc/miscellaneous_transformations.html#cvtcolor
-rgb2hsv <- function( RGB )
+# FIXME: We still need to make sure this function works well.
+rgb2hsv <- function()
 {
-    maxRGB = apply(RGB, 1, max)
-    minRGB = apply(RGB, 1, min) #1 is for function apply on row.
-    maxMinDelta = maxRGB-minRGB
+    #FIXME: We should check if environment is valid.
+    if ( ! "RGB" %in% ls(envir=as.environment(refArgs)) )
+        stop (passByRefMessage("The rgb2hsv method needs RGB"))
+    RGB = get("RGB", envir=as.environment(refArgs))
 
-    V = maxRGB
+    if ( dim(RGB)[2] != 3 )
+        stop ("The rgb var must have 3 dimensions")
 
-    S = maxMinDelta / maxRGB
+    V = apply(RGB, 1, max) # V is MAX
+    maxMinDelta = V - apply(RGB, 1, min) # V - MIN
+
+    S = maxMinDelta / V
     S[ is.infinite(S) ] = 0 # Inf is the result of dividing by 0
 
-    Coef = (maxRGB == RGB) # Results in Nx3 boolean matrix
+    Coef = (V == RGB) # Results in Nx3 boolean matrix
 
+    # In R FALSE*num = 0 :)
     H = 60 * ( Coef[,1]*((RGB[2]-RGB[3])/maxMinDelta)
                + Coef[,2]*(((RGB[3]-RGB[1])+2)/maxMinDelta)
                + Coef[,3]*(((RGB[1]-RGB[2])+4)/maxMinDelta) )
+
+    rm(maxMinDelta, Coef) # Keep memory usage down.
+    rm("RGB", envir=as.environment(refArgs))
 
     return (cbind(H,S,V))
 }
