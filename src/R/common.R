2ce61f24004bb9783a7109d1796a4bdbce7678e0
Joel Andres Granados <joel.granados@gmail.com>
2012-03-15 17:37:33 +0100

    Remove unneeded comment

--- src/R/common.R
+++ src/R/common.R
@@ -306,7 +306,7 @@ generate.MaskVideo <- function(self, outdir=NULL, G=NULL, together=F)
     # Remove temp dir.
     unlink ( tmpdir, recursive=T, force=T )
     return (result)
-} #FIXME: make sure it returns correctly
+}
 
 # This is annoying: tempdir() will give current session tempdir. This is used
 # by the session and cannot be erased. Don't know how to tmpdir in R :(.
