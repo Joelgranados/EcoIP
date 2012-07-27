605ba7b4261b175223113b6b653dfbd9c0c7ad0f
Joel Andres Granados <joel.granados@gmail.com>
2012-07-27 10:31:04 +0200

    Create a way for easy install. ecoip_install.R.

---
+++ src/R/ecoip_install.R
@@ -0,0 +1,35 @@
+# Copyright (C) 2012 Joel Granados <joel.granados@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+eip.install <- function ()
+{
+    # Define some vars
+    eip.trz = "EcoIP_0.1-20120726.tar.gz"
+    eip.from = paste("http://github.com/downloads/Joelgranados/EcoIP/",
+                     eip.trz, sep="")
+    eip.to = paste ( "~/", eip.trz, sep="" )
+
+    print(eip.from)
+    print(eip.to)
+    # Download source.
+    download.file ( eip.from, eip.to, method="wget" )
+
+    # Install source.
+    install.packages( eip.to )
+
+    # Clean up
+    unlink ( eip.to )
+}
